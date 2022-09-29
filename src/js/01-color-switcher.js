edb573d5b0fe4da0c6affff538439a1cea786eb6
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2022-09-29 14:51:41 +0300

    Task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtnRef = document.querySelector('[data-start]');
+const stopBtnRef = document.querySelector('[data-stop]');
+const bodyRef = document.querySelector('body');
+
+startBtnRef.addEventListener('click', onStartBtnClick);
+stopBtnRef.addEventListener('click', onStopBtnClick);
+
+let intervalId = null;
+
+function onStartBtnClick() {
+  intervalId = setInterval(() => {
+    bodyRef.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+  startBtnRef.setAttribute('disabled', 'disabled');
+  stopBtnRef.removeAttribute('disabled');
+}
+
+function onStopBtnClick() {
+  clearInterval(intervalId);
+  stopBtnRef.setAttribute('disabled', 'disabled');
+  startBtnRef.removeAttribute('disabled');
+}
