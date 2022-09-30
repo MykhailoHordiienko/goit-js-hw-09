c32e8f82b897a9ee0507990a1e816e8656629e68
Mykhailo Hordiienko <gordienkomichael191@gmail.com>
2022-09-30 08:31:25 +0300

    test 2

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,44 +1,44 @@
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import 'flatpickr/dist/flatpickr.min.css';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import 'flatpickr/dist/flatpickr.min.css';
 
-const notifyOptions = {
-  position: 'center-center',
-  backOverlay: true,
-  clickToClose: true,
-};
+// const notifyOptions = {
+//   position: 'center-center',
+//   backOverlay: true,
+//   clickToClose: true,
+// };
 
-let delay = 0;
-let step = 0;
-let amount = 0;
-let delayArr = [];
-window.alert('wefsdf');
+// let delay = 0;
+// let step = 0;
+// let amount = 0;
+// let delayArr = [];
+// window.alert('wefsdf');
 
-const delayRef = document.querySelector('[name="delay"]');
-const stepRef = document.querySelector('[name="step"]');
-const amountRef = document.querySelector('[name="amount"]');
-const submitRef = document.querySelector('[type="submit"]');
+// const delayRef = document.querySelector('[name="delay"]');
+// const stepRef = document.querySelector('[name="step"]');
+// const amountRef = document.querySelector('[name="amount"]');
+// const submitRef = document.querySelector('[type="submit"]');
 
-delayRef.addEventListener('input', ev => (delay = ev.target.value));
-stepRef.addEventListener('input', ev => (step = ev.target.value));
-amountRef.addEventListener('input', ev => (amount = ev.target.value));
-submitRef.addEventListener('click', onSubmitClick);
+// delayRef.addEventListener('input', ev => (delay = ev.target.value));
+// stepRef.addEventListener('input', ev => (step = ev.target.value));
+// amountRef.addEventListener('input', ev => (amount = ev.target.value));
+// submitRef.addEventListener('click', onSubmitClick);
 
-function onSubmitClick(evnt) {
-  evnt.preventDefault();
-  console.log('submit');
-  for (let i = 1; i <= amount; i++) {
-    let deleyFinal = Number(delay) + Number(step);
-    delayArr.push(step);
+// function onSubmitClick(evnt) {
+//   evnt.preventDefault();
+//   console.log('submit');
+//   for (let i = 1; i <= amount; i++) {
+//     let deleyFinal = Number(delay) + Number(step);
+//     delayArr.push(step);
 
-    createPromise(i, deleyFinal);
-  }
-}
+//     createPromise(i, deleyFinal);
+//   }
+// }
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  } else {
-    // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  }
-}
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   } else {
+//     // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   }
+// }
